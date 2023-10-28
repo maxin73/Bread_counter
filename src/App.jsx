6e295e90bc195a0fd8a66a42fe6da769c73ac0a2
Nami Miyashiro <nami.miyashiro@gmail.com>
2023-10-28 00:15:33 -0700

    update display rule

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,30 @@ function App() {
   const today = new Date();
   const month = today.getMonth() +1;
   const date = today.getDate();
+  const dayOfWeek = today.getDay();
+
+  const breadDisplayRules1 = {
+    0: ['メロンパン - CC', 'メロンパン - ほうじ'],
+    3: ['メロンパン - CC'],
+    5: ['メロンパン - ほうじ'],
+    6: ['メロンパン - CC', 'メロンパン - ほうじ'],
+  };
+
+  const breadDisplayRulesMocha = {
+    0: ['メロンパン - モカ'],
+    4: ['メロンパン - モカ'],
+    6: ['メロンパン - モカ'],
+  };
+
+    const breadDisplayRules2 = {
+    6: [
+    'フルーツサンド(土曜限定)',
+    'たまごサンド(土曜限定)',
+    'ゆず七味(土曜限定)',
+    'カツサンド(土曜限定)',
+    'えびカツサンド(土曜限定)',
+    ],
+  };
 
   const initialBreadData = [
     { name: 'カレーパン(牛肉)', count: 0, genre: '1' },
@@ -15,12 +39,12 @@ function App() {
     { name: 'ハムマヨパン', count: 0, genre: '1' },
     { name: 'コーンマヨパン', count: 0, genre: '1' },
     { name: 'たこ焼きパン', count: 0, genre: '1' },
-    { name: 'メロンパン - バニラ', count: 0, genre: '2' },
-    { name: 'メロンパン - 抹茶', count: 0, genre: '2' },
-    { name: 'メロンパン - モカ', count: 0, genre: '2' },
+    { name: 'メロンパン - バニラ', count: 0, genre: '1' },
+    { name: 'メロンパン - 抹茶', count: 0, genre: '1' },
+    { name: 'メロンパン - モカ', count: 0, },
     { name: 'メロンパン - EG', count: 0, genre: '2' },
-    { name: 'メロンパン - CC', count: 0, genre: '2' },
-    { name: 'メロンパン - ほうじ', count: 0, genre: '2' },
+    { name: 'メロンパン - CC', count: 0, },
+    { name: 'メロンパン - ほうじ', count: 0, },
     { name: 'コロネ', count: 0, genre: '3' },
     { name: 'あんぱん', count: 0, genre: '3' },
     { name: 'メープルローフ', count: 0, genre: '3' },
@@ -33,12 +57,10 @@ function App() {
     { name: 'みそハバーティ', count: 0, genre: '4' },
     { name: 'ゆずねぎチーズ', count: 0, genre: '4' },
     { name: 'アップルシナモン', count: 0, genre: '4' },
-    { name: 'もちロールきなこ', count: 0, genre: '4' },
-    { name: 'もちロール黒ゴマ', count: 0, genre: '4' },
     { name: 'ゆず抹茶スワール', count: 0, genre: '4' },
     { name: 'オレンジチョコスワール', count: 0, genre: '4' },
-    { name: 'ゆずデニッシュ', count: 0, genre: '5' },
-    { name: 'もちあんこパイ', count: 0, genre: '5' },
+    { name: 'ゆずデニッシュ', count: 0, genre: '4' },
+    { name: 'もちあんこパイ', count: 0, genre: '4' },
     { name: 'もちケーキプレーン', count: 0, genre: '5' },
     { name: 'もちケーキ抹茶', count: 0, genre: '5' },
     { name: 'ミニもち 抹茶', count: 0, genre: '5' },
@@ -47,26 +69,25 @@ function App() {
     { name: 'ミニもち チョコ', count: 0, genre: '5' },
     { name: 'ミニもち コーヒー', count: 0, genre: '5' },
     { name: 'ミニもち あんこ', count: 0, genre: '5' },
-    { name: 'たまごコッペ', count: 0, genre: '6' },
-    { name: 'コロッケコッペ', count: 0, genre: '6' },
-    { name: 'フルーツサンド(土曜限定)', count: 0, genre: '6' },
-    { name: 'たまごサンド(土曜限定)', count: 0, genre: '6' },
-    { name: 'ゆず七味(土曜限定)', count: 0, genre: '6' },
-    { name: 'カツサンド(土曜限定)', count: 0, genre: '6' },
-    { name: 'えびカツサンド(土曜限定)', count: 0, genre: '6' },
-    { name: 'モンブランタルト', count: 0, genre: '7' },
-    { name: 'イチゴショート', count: 0, genre: '7' },
-    { name: 'イチゴ大福', count: 0, genre: '7' },
-    { name: 'どら焼き', count: 0, genre: '7' },
-    { name: 'ナナイモバー抹茶', count: 0, genre: '7' },
-    { name: 'ナナイモバー黒ゴマ', count: 0, genre: '7' },
-    { name: '柚子レモンバー', count: 0, genre: '7' },
-    { name: 'ケーキポップ', count: 0, genre: '7' },
-    { name: 'プリン', count: 0, genre: '7' },
-    { name: 'チーズスフレ', count: 0, genre: '7' },
-    { name: 'Day Old MCR', count: 0, genre: '7' },
-    { name: 'Day Old 山食', count: 0, genre: '7' },
-    { name: 'Day Old ディナーロール', count: 0, genre: '7' },
+    { name: 'たまごコッペ', count: 0, genre: '5' },
+    { name: 'フルーツサンド(土曜限定)', count: 0, },
+    { name: 'たまごサンド(土曜限定)', count: 0, },
+    { name: 'ゆず七味(土曜限定)', count: 0, },
+    { name: 'カツサンド(土曜限定)', count: 0, },
+    { name: 'えびカツサンド(土曜限定)', count: 0, },
+    { name: 'モンブランタルト', count: 0, genre: '6' },
+    { name: 'イチゴショート', count: 0, genre: '6' },
+    { name: 'イチゴ大福', count: 0, genre: '6' },
+    { name: 'どら焼き', count: 0, genre: '6' },
+    { name: 'ナナイモバー抹茶', count: 0, genre: '6' },
+    { name: 'ナナイモバー黒ゴマ', count: 0, genre: '6' },
+    { name: '柚子レモンバー', count: 0, genre: '6' },
+    { name: 'ケーキポップ', count: 0, genre: '6' },
+    { name: 'プリン', count: 0, genre: '6' },
+    { name: 'チーズスフレ', count: 0, genre: '6' },
+    { name: 'Day Old MCR', count: 0, genre: '6' },
+    { name: 'Day Old 山食', count: 0, genre: '6' },
+    { name: 'Day Old ディナーロール', count: 0, genre: '6' },
   ];
   
   const [breadData, setBreadData] = useState(initialBreadData);
@@ -89,7 +110,18 @@ function App() {
   const genre4 = breadData.filter((bread) => bread.genre === "4");
   const genre5 = breadData.filter((bread) => bread.genre === "5");
   const genre6 = breadData.filter((bread) => bread.genre === "6");
-  const genre7 = breadData.filter((bread) => bread.genre === "7");
+
+  const filteredBreadData1 = breadDisplayRules1[dayOfWeek]
+    ? breadData.filter((bread) => breadDisplayRules1[dayOfWeek].includes(bread.name))
+    : [];
+
+  const filteredBreadDataMocha = breadDisplayRulesMocha[dayOfWeek]
+    ? breadData.filter((bread) => breadDisplayRulesMocha[dayOfWeek].includes(bread.name))
+    : [];
+
+  const filteredBreadData2 = breadDisplayRules2[dayOfWeek]
+    ? breadData.filter((bread) => breadDisplayRules2[dayOfWeek].includes(bread.name))
+    : [];
 
   return (
     <>
@@ -111,7 +143,25 @@ function App() {
             ))}
           </div>
           <div className="genre2">
+            {filteredBreadDataMocha.map((bread) => (
+              <Counter 
+                key={bread.name}
+                bread={bread.name}
+                count={bread.count}
+                onIncrement={() => updateBreadCount(bread.name, 1)}
+                onDecrement={() => updateBreadCount(bread.name, -1)}
+              />
+            ))}
             {genre2.map((bread) => (
+              <Counter 
+                key={bread.name}
+                bread={bread.name}
+                count={bread.count}
+                onIncrement={() => updateBreadCount(bread.name, 1)}
+                onDecrement={() => updateBreadCount(bread.name, -1)}
+              />
+            ))}
+            {filteredBreadData1.map((bread) => (
               <Counter 
                 key={bread.name}
                 bread={bread.name}
@@ -155,6 +205,15 @@ function App() {
                 onDecrement={() => updateBreadCount(bread.name, -1)}
               />
             ))}
+            {filteredBreadData2.map((bread) => (
+              <Counter 
+                key={bread.name}
+                bread={bread.name}
+                count={bread.count}
+                onIncrement={() => updateBreadCount(bread.name, 1)}
+                onDecrement={() => updateBreadCount(bread.name, -1)}
+              />
+            ))}
           </div>
           <div className="genre6">
             {genre6.map((bread) => (
@@ -167,20 +226,9 @@ function App() {
               />
             ))}
           </div>
-          <div className="genre7">
-            {genre7.map((bread) => (
-              <Counter 
-                key={bread.name}
-                bread={bread.name}
-                count={bread.count}
-                onIncrement={() => updateBreadCount(bread.name, 1)}
-                onDecrement={() => updateBreadCount(bread.name, -1)}
-              />
-            ))}
-          </div>
         </div>
       </section>
-      <ScreenshotCapture screenshotRef={screenshotRef} />
+      {/* <ScreenshotCapture screenshotRef={screenshotRef} /> */}
     </div>
     </>
   );
